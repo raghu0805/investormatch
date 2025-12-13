96ea54a7c4f7e76b399010e72d3e2c35785710b7
PRASANNA07-11-2005 <imraghu0805@gmail.com>
2025-12-13 21:32:58 +0530

    host ready

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api=axios.create({
-    baseURL:"https://investmatch-backend.onrender.com/api"
+    baseURL:"https://investormatch-backend-yn2k.onrender.com/api"
 });
 
 api.interceptors.request.use((config)=>{
