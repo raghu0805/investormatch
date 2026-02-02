ed5258f78d2105deeae26ff71581c97029ab0476
PRASANNA07-11-2005 <imraghu0805@gmail.com>
2026-02-02 12:22:49 +0530

    n8n bug fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+console.log("VITE_GOOGLE_CLIENT_ID:", clientId);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={clientId}>
       <DataContext>
         <BrowserRouter>
           <App />
